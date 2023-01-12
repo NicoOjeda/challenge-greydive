b9e19ced1f1ae185f62dd9f2bd7366b43e3c4a42
Nico ojeda <nico_ojeda86@hotmail.com>
2023-01-12 03:14:57 -0300

    se define y agrega footer a layout

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Footer.css'
+
+export default function Footer() {
+  return (
+    <div className='Footer-box'>
+        <a className='Footer-a' href="https://www.greydive.com/">greydive.</a>
+        <p className='Footer-p'>© CRONVERSION SRL</p>
+        <a className='Footer-l' href="https://github.com/NicoOjeda/challenge-greydive">Github/greydive</a>
+    </div>
+  )
+}
