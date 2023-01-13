763af4889b5ef34e637c2e052114d89f04918a9e
Nico ojeda <nico_ojeda86@hotmail.com>
2023-01-12 23:22:28 -0300

    se modifica input date para limitar fechas

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 import { useEffect, useState } from 'react'
 import app from '../credenciales'
 import {getFirestore, collection, addDoc} from 'firebase/firestore'
-import { useNavigate } from 'react-router';
 import '../styles/Form.css'
 import Swal from 'sweetalert2'
+import format from 'date-fns/format'
 
 export default function Form() {
+    
 
     const [data, setData] = useState([])
     const db = getFirestore(app)
-    const navigate = useNavigate()
+    let fe = new Date()
+    let feSet = format(fe, 'yyyy-MM-dd')
+    console.log(feSet);
 
     const [valorInicial, setValorInicial] = useState ({
         full_name:'',
@@ -66,6 +69,12 @@ const input = (user)=> (
         <input  className='Form-input' id={user.name} name={user.name} type={user.type} onChange={capturoInput} required />
     </div>
 )  
+const date = (user)=> (
+    <div className='Form-inputbox'>
+        <label className='Form-label' for={user.name}>{user.label}</label>
+        <input type={user.type} className='Form-input' id={user.name} name={user.name}  max={feSet} onChange={capturoInput} required />
+    </div>
+)  
 const check = (user)=> (
     <div className='Form-check'>
         <label for={user.name}>{user.label}</label>
@@ -81,10 +90,10 @@ const submit = (user)=> (
 const select = (user)=> (
     <div className='Form-inputbox'>
         <label  for={user.name}>{user.label}</label>
-        <select className='Form-input' name={user.name} onChange={capturoInput}>
-            <option disabled value='' defaultValue>Selecciona un país</option>
+        <select className='Form-input' name={user.name} onChange={capturoInput} required={user.required} defaultValue="">
+            <option value="" >Selecciona un país</option>
         {
-            user.options.map(option => <option  value={option.value}>{option.label}</option>)
+            user.options.map(option => <option  value={option.value} >{option.label}</option>)
         }
         </select>
     </div>
@@ -102,6 +111,7 @@ const select = (user)=> (
                 select(user) 
                     : user.type === "submit" ? submit(user)
                     : user.type === "checkbox" ? check(user)
+                    : user.type === "date" ? date(user)
                     : input(user)
                 )
             }
