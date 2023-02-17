b9dddda2b84d1b2321b1c1bd39c278f13fc4b69b
Nico ojeda <nico_ojeda86@hotmail.com>
2023-02-16 22:40:46 -0300

    se modifica nombre de challenge y links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ import '../styles/Footer.css'
 export default function Footer() {
   return (
     <div className='Footer-box'>
-        <a className='Footer-a' href="https://www.greydive.com/">greydive.</a>
-        <p className='Footer-p'>© CRONVERSION SRL</p>
-        <a className='Footer-l' href="https://github.com/NicoOjeda/challenge-greydive">Github/greydive</a>
+        <a className='Footer-a' href="">Challenge React-Firebase</a>
+        <p className='Footer-p'>© Nico Ojeda</p>
+        <a className='Footer-l' href="https://github.com/NicoOjeda/challenge-greydive">Github/Challenge</a>
     </div>
   )
 }
